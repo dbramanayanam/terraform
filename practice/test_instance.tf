01782ac62053330eafcc9a56352a811dd0f2c03f
Dinesh babu <dbramanayanam@gmail.com>
2024-04-06 11:20:50 +0400

    first commit

---
+++ practice/test_instance.tf
@@ -0,0 +1,33 @@
+resource "aws_instance" "instance"{
+  ami                     = var.ami_id
+  instance_type           = var.instance_type
+  vpc_security_group_ids  = [aws_security_group.allow_tls.id]
+  tags = {
+      Name = "test-instance"
+  }
+}
+
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls"
+  description = "Allow TLS inbound traffic and all outbound traffic"
+  #vpc_id      = aws_vpc.main.id
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
+  security_group_id = aws_security_group.allow_tls.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 0
+  ip_protocol       = "tcp"
+  to_port           = 65535
+}
+
+
+resource "aws_vpc_security_group_egress_rule" "allow_tls_ipv4" {
+  security_group_id = aws_security_group.allow_tls.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1" 
+}
---
+++ practice/variable.tf
@@ -0,0 +1,12 @@
+variable "ami_id" {
+  #type        = string
+  default     = "ami-0f3c7d07486cad139"
+  description = "Devops-practice image id"
+}
+
+variable "instance_type" {
+  #type        = string
+  default     = "t2.micro"
+  description = "instace type"
+}
+
