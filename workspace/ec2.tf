b2c86a71aa0b4431af01ccc17443fb0c0c5996e8
Dinesh babu <dbramanayanam@gmail.com>
2024-04-07 17:39:38 +0400

    Added terraform workspace

---
+++ workspace/ec2.tf
@@ -0,0 +1,12 @@
+resource "aws_instance" "instance" {
+  ami                     = var.ami_id
+  #for_each                = var.tags
+  vpc_security_group_ids  = [aws_security_group.allow_tls.id]
+  #count = 11
+  #count = length(var.tags)
+  instance_type  = lookup(var.instance_type, terraform.workspace)
+
+  tags = {
+    Name = "Hello Workspace"
+   }
+}
